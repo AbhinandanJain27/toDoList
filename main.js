b6c74033a492a678c9c4370fcda876afdc890533
Abhinandan Jain <w191672@WL-CKQ0BG3>
2025-03-03 13:06:18 +0530

    Initial Packaging

---
+++ main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+    });
+    win.loadFile(path.join(__dirname, 'dist/browser/index.html'))
+        .catch(err => {
+            console.error('Failed to load the index.html file:', err);
+        });
+
+    // Optional: Open DevTools for debugging
+    win.webContents.openDevTools();
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
